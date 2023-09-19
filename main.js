1370fa2a67533fbfc1463b3d0d70cc1085b5f322
stian88 <rodriguezjs88@gmail.com>
2023-09-19 18:18:49 -0300

    commit
    Acomodando codigo

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 import './style.css'
 
+   
+function stop() {
 
+    console.info("STOP"); 
+  }
+
+   function timbre() {
+
+     var timbre = document.getElementById("Timbre");
+     timbre.play();
+   }
+
+   const Ascensor = document.getElementById("Ascensor");
+
+   function moverAscensor(piso) {               
+        cerrarPuerta();
+
+     const pixelPisos = 80; // Altura de cada piso en píxeles
+     const pisoDestino = piso * pixelPisos;
+     Ascensor.style.bottom = pisoDestino + "px";    
+         
+     setTimeout(abrirPuerta, 3000);
+  }   
+      
+   function cerrarPuerta() {
+             const cerrar = document.getElementById("Ascensor");
+             cerrar.style.backgroundColor = "gray";
+        }
+   
+   function abrirPuerta() {
+             const abrir = document.getElementById("Ascensor");
+             abrir.style.backgroundColor = "Yellow";
+  }
 
 
 
